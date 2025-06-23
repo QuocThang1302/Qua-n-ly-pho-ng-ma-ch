8a6538f68cb978c629b6de8451eb5e369808a449
pnson1322 <pnson22@gmail.com>
2025-06-23 14:40:38 +0700

    xóa patient_add và làm 1 nửa patient_detail

--- src/main/java/com/example/controllers/PatientController.java
+++ src/main/java/com/example/controllers/PatientController.java
@@ -3,11 +3,22 @@ package com.example.controllers;
 import com.example.helper.NavigationHelper;
 import com.example.model.PatientModel;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class PatientController {
     @FXML
@@ -24,8 +35,6 @@ public class PatientController {
     private TableColumn<PatientModel, String> birthCol;
     @FXML
     private TextField tfSearch;
-    @FXML
-    private ImageView ivAdd;
 
     @FXML
     public void initialize(){
@@ -39,8 +48,36 @@ public class PatientController {
             ((Region) tvPatient.lookup("TableHeaderRow")).setPrefHeight(45);
         });
 
-        ivAdd.setOnMouseClicked(event -> {
-            //NavigationHelper.setContent(contentArea, "/views/dashboard.fxml");
+        tvPatient.setOnMouseClicked((event) -> {
+           if (event.getClickCount() == 2){
+                PatientModel patientModel = tvPatient.getSelectionModel().getSelectedItem();
+                if (patientModel != null){
+                    showPatientDetailPopUp(patientModel);
+                }
+           }
         });
+
+        PatientModel patientModel = tvPatient.getSelectionModel().getSelectedItem();
+        showPatientDetailPopUp(patientModel);
+    }
+
+    private void showPatientDetailPopUp(PatientModel patientModel) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/patient_detail_dialog.fxml"));
+            Parent root = loader.load();
+
+            // Lấy controller để truyền dữ liệu
+            PatientDetailDialogController controller = loader.getController();
+            controller.setPatient(patientModel);
+
+            // Tạo stage mới (window mới)
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Chi tiết bệnh nhân");
+            dialogStage.initModality(Modality.APPLICATION_MODAL); // chặn tương tác với window chính
+            dialogStage.setScene(new Scene(root));
+            dialogStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
