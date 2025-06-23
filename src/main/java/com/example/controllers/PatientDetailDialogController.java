fa0f5925f45052bbd032afdb25c2a7c809226b2e
pnson1322 <pnson22@gmail.com>
2025-06-24 01:53:27 +0700

    xong ui patient_detail_dialog.fxml

--- src/main/java/com/example/controllers/PatientDetailDialogController.java
+++ src/main/java/com/example/controllers/PatientDetailDialogController.java
@@ -1,14 +1,18 @@
 package com.example.controllers;
 
+import com.example.model.MedicalReportModel;
 import com.example.model.PatientModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.StringConverter;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -28,15 +32,29 @@ public class PatientDetailDialogController {
     /*Cách in ra ngày theo định dạng dd/mm/yyyy nè
       LocalDate date = dpBirth.getValue();
       String format = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));*/
+    @FXML
+    private TableView<MedicalReportModel> tvKhamBenh;
+    @FXML
+    private TableColumn<MedicalReportModel, String> dateCol;
+    @FXML
+    private TableColumn<MedicalReportModel, String> resultCol;
+    @FXML
+    private TableColumn<MedicalReportModel, String> costCol;
+    @FXML
+    private TableColumn<MedicalReportModel, String> treatCol;
+    @FXML
+    private TableColumn<MedicalReportModel, String> doctorCol;
 
     @FXML
     public void initialize() {
+        // code của toggle button
         ToggleGroup genderGroup = new ToggleGroup();
         btnMale.setToggleGroup(genderGroup);
         btnFemale.setToggleGroup(genderGroup);
 
         btnMale.setSelected(true);
-
+        
+        // code của date picker
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         dpBirth.setConverter(new StringConverter<LocalDate>() {
@@ -50,6 +68,42 @@ public class PatientDetailDialogController {
                 return (string != null && !string.isEmpty()) ? LocalDate.parse(string, formatter) : null;
             }
         });
+        
+        // code của table view
+        dateCol.prefWidthProperty().bind(tvKhamBenh.widthProperty().multiply(0.2));
+        resultCol.prefWidthProperty().bind(tvKhamBenh.widthProperty().multiply(0.2));
+        treatCol.prefWidthProperty().bind(tvKhamBenh.widthProperty().multiply(0.2));
+        costCol.prefWidthProperty().bind(tvKhamBenh.widthProperty().multiply(0.2));
+        doctorCol.prefWidthProperty().bind(tvKhamBenh.widthProperty().multiply(0.2));
+
+        tvKhamBenh.setOnMouseClicked((event) -> {
+            if (event.getClickCount() == 2) {
+                MedicalReportModel medicalReportModel = tvKhamBenh.getSelectionModel().getSelectedItem();
+                if (medicalReportModel != null) {
+                    showMedicalReportPopUp(medicalReportModel);
+                }
+            }
+        });
+    }
+
+    private void showMedicalReportPopUp(MedicalReportModel medicalReportModel) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/medical_report.fxml"));
+            Parent root = loader.load();
+
+            // Lấy controller để truyền dữ liệu
+            MedicalReportController controller = loader.getController();
+            controller.setData(medicalReportModel, medicalReportModel.getHoaDon());
+
+            // Tạo stage mới (window mới)
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Phiếu khám bệnh");
+            dialogStage.initModality(Modality.APPLICATION_MODAL); // chặn tương tác với window chính
+            dialogStage.setScene(new Scene(root));
+            dialogStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setPatient(PatientModel patient) {
